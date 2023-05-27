837d44b8e0c91bba9260eee379b84b159848455b
noklash <123834419+noklash@users.noreply.github.com>
2023-05-28 02:27:00 +0300

    fixed the local server bug by running the command in the root folder directory

--- Noklash-may-portfolio/src/components/Navbar.jsx
+++ Noklash-may-portfolio/src/components/Navbar.jsx
@@ -5,7 +5,13 @@ import "./Navbar.css"
 export default function Navbar(){
     return (
         <div>
-           
+          <h2>Ken.mild</h2> 
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
 
         </div>
     )
