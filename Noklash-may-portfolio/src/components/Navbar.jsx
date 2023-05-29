19e0d06d2f8aa46a03a49b08517cf049cbc728a8
noklash <123834419+noklash@users.noreply.github.com>
2023-05-29 23:19:30 +0300

    rearraged the navbar component

--- Noklash-may-portfolio/src/components/Navbar.jsx
+++ Noklash-may-portfolio/src/components/Navbar.jsx
@@ -3,36 +3,21 @@ import "./Navbar.css"
 
 
 export default function Navbar(){
+
+    
   
     return (
         <div>
-          <nav class="navbar navbar-expand-lg bg-body- text-white">
-              <div class="container-fluid">
-                <a class="navbar-brand text-white" href="#">Ken.mild</a>
-                <button class="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon "></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav">
-                    <li class="nav-item">
-                      <a class="nav-link text-white" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link text-white" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link text-white" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link text-white">Portfolio</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link text-white">Contact</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </nav> 
+          <nav className="nav-bar shadow">
+            <h3 className="header">Noklash</h3>
+            <ul className="nav-ul">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+          <hr className="mt-2"/>
 
         </div>
     )
